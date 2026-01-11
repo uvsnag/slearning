908308367b6234d4b3d96df9cda13d16ba311847
snag <sangnv046@gmail.com>
2026-01-11 18:11:38 +0700

    Updates

---
+++ _next/static/HghDwUA7Y7d7e-rccvIL8/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/slearning//_next/:path+",
+        "destination": "/slearning/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
